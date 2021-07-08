e6c3a36cd4a19fba4c88d38d4b97651a100d628d
Bruno <brunoco56@hotmail.com>
2021-07-08 14:19:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SistemaEscolar
+{
+    public class Program
+    {     
+
+        static void Main(string[] args)
+        {
+
+            int resposta = ObterOpcaoUsuario();
+
+            while (resposta != 0)
+            {
+                switch (resposta)
+                {
+
+                    case 1:
+                        Console.WriteLine("numero 1");
+                        //codigo
+                        break;
+                    case 2:
+                      
+                        Console.WriteLine("numero 2");
+                        //codigo
+                        break;
+                    case 3:
+                      
+                        Console.WriteLine("numero 3");
+                        //codigo
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            
+                resposta = ObterOpcaoUsuario();
+            }
+
+        }
+
+        private static int ObterOpcaoUsuario()
+        {
+
+            Console.WriteLine("Informe a opção desejada: ");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1 - Inserir Novo Aluno.");
+            Console.WriteLine("2 - Listar Alunos.");
+            Console.WriteLine("3 - Calcular média Geral.");
+            Console.WriteLine("0 - Sair ");
+            Console.WriteLine("-------------------------------");
+            int resposta = int.Parse(Console.ReadLine());
+            return resposta;
+
+        }
+    }
+}
